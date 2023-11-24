b830cbef9c1504c60ac92401bda836928eec5899
hendisantika <hendisantika@yahoo.co.id>
2023-11-25 06:15:15 +0700

    Add createOrder in OrderController in Order Service 🫸🌀✏️📗🐧🐳

--- order-service/src/main/java/com/hendisantika/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/hendisantika/orderservice/controller/OrderController.java
@@ -1,9 +1,13 @@
 package com.hendisantika.orderservice.controller;
 
+import com.hendisantika.orderservice.dto.CustomerOrder;
 import com.hendisantika.orderservice.dto.OrderEvent;
+import com.hendisantika.orderservice.entity.Order;
 import com.hendisantika.orderservice.entity.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +29,22 @@ public class OrderController {
     private final OrderRepository orderRepository;
 
     private final KafkaTemplate<String, OrderEvent> kafkaTemplate;
+
+    @PostMapping("/orders")
+    public void createOrder(@RequestBody CustomerOrder customerOrder) {
+        Order order = new Order();
+        order.setAmount(customerOrder.getAmount());
+        order.setItem(customerOrder.getItem());
+        order.setQuantity(customerOrder.getQuantity());
+        order.setStatus("CREATED");
+        order = orderRepository.save(order);
+
+        customerOrder.setOrderId(order.getId());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setOrder(customerOrder);
+        orderEvent.setType("ORDER_CREATED");
+
+        kafkaTemplate.send("orders-topic", orderEvent);
+    }
 }
