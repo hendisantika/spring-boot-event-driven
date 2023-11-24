b1754f24e0eed7280bfdb355a8e452652e4a3d73
hendisantika <hendisantika@yahoo.co.id>
2023-11-25 06:12:08 +0700

    Add OrderEvent in Order Service 🫸🌀✏️📗🐧🐳

---
+++ order-service/src/main/java/com/hendisantika/orderservice/dto/OrderEvent.java
@@ -0,0 +1,24 @@
+package com.hendisantika.orderservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-event-driven
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/25/23
+ * Time: 06:11
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class OrderEvent {
+
+    private String type;
+
+    private CustomerOrder order;
+
+}
