cd586d585ee1740ad390d7505d88f0f22e3db8c2
hendisantika <hendisantika@yahoo.co.id>
2023-11-25 06:22:27 +0700

    Add processPayment in Payment Controller in Payment Service 🫸🌀✏️📗🐧🐳

--- payment-service/src/main/java/com/hendisantika/paymentservice/controller/PaymentController.java
+++ payment-service/src/main/java/com/hendisantika/paymentservice/controller/PaymentController.java
@@ -1,7 +1,15 @@
 package com.hendisantika.paymentservice.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.paymentservice.dto.CustomerOrder;
+import com.hendisantika.paymentservice.dto.OrderEvent;
+import com.hendisantika.paymentservice.entity.Payment;
 import com.hendisantika.paymentservice.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -16,6 +24,22 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class PaymentController {
     private final PaymentRepository paymentRepository;
+
+    @KafkaListener(topics = "orders-topic", groupId = "orders-group")
+    public void processPayment(String event) throws JsonProcessingException {
+        log.info("Recieved event for payment " + event);
+        OrderEvent orderEvent = new ObjectMapper().readValue(event, OrderEvent.class);
+
+        CustomerOrder order = orderEvent.getOrder();
+
+        Payment payment = new Payment();
+        payment.setAmount(order.getAmount());
+        payment.setMode(order.getPaymentMode());
+        payment.setOrderId(order.getOrderId());
+        payment.setStatus("SUCCESS");
+        paymentRepository.save(payment);
+    }
 }
